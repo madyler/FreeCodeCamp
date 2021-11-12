07a79c9b65c3f095ad0b2fd6efb918232cfdb045
madyler <77465333+madyler@users.noreply.github.com>
2021-11-13 01:39:25 +0300

    Add files via upload

---
+++ 089.js
@@ -0,0 +1,23 @@
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    {
+      artist: "Eminem",
+      title: "8 mile",
+      release_year: 2002,
+      formats: [
+        "CD",
+        "DVD",
+        "Blue-ray"
+      ]
+    }
+  ];
